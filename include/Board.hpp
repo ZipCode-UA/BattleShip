c3d4898f0eb97d7a5db98bdcac52a9eeacc20d63
Lucas Ergh <lucasergh@gmail.com>
2025-10-06 22:33:05 -0400

    Add CellTypes enum

--- include/Board.hpp
+++ include/Board.hpp
@@ -5,6 +5,18 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
+/**
+ * @enum CellTypes
+ *
+ * @brief Represents four possible states a cell can be in
+ */
+enum CellTypes {
+    Empty,
+    Ship,
+    Hit,
+    Miss
+};
+
 class Board {
 
 };
