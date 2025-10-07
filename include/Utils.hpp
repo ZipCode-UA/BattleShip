0a057060f00fa8db4c1bb4d192c1f219f38c5f6f
Lucas Ergh <lucasergh@gmail.com>
2025-10-06 23:55:17 -0400

    Move CellTypes enum to Utils.hpp

---
+++ include/Utils.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file Utils.hpp
+ */
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+/**
+ * @enum CellTypes
+ *
+ * @brief Represents four possible states a cell can be in
+ */
+enum CellTypes {
+    Empty,
+    Ship,
+    Hit,
+    Miss
+};
+
+#endif
