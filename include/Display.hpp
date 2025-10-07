a4d4bd6c1cc886bb1be64078d724e50e2efdb94e
Lucas Ergh <lucasergh@gmail.com>
2025-10-06 22:51:13 -0400

    Add doxygen comment to DrawBanner function

--- include/Display.hpp
+++ include/Display.hpp
@@ -5,6 +5,11 @@
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
 
+/**
+ * DrawBanner
+ * 
+ * @brief Draws an ascii title banner to standard out
+ */
 void DrawBanner();
 
 #endif
--- src/Display.cpp
+++ src/Display.cpp
@@ -5,6 +5,11 @@
 #include "Display.hpp"
 #include <iostream>
 
+/**
+ * DrawBanner
+ * 
+ * @brief Draws an ascii title banner to standard out
+ */
 void DrawBanner() {
     std::cout << "Welcome to BattleShip!\n";
 }
