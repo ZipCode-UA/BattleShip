5ab4191d606f8f9a47af805b759ed277f74d7454
Lucas Ergh <lucasergh@gmail.com>
2025-10-07 00:50:45 -0400

    Add Input functions

--- include/Input.hpp
+++ include/Input.hpp
@@ -5,4 +5,14 @@
 #ifndef INPUT_HPP
 #define INPUT_HPP
 
+/**
+ * @brief Prompts, parses, and validates cell user choses to shoot
+ */
+void getShotCell();
+
+/**
+ * @brief Prompts, parses, and validates user ship placement
+ */
+void placeShips();
+
 #endif
--- src/Input.cpp
+++ src/Input.cpp
@@ -3,3 +3,16 @@
  */
 
 #include "Input.hpp"
+/**
+ * @brief Prompts, parses, and validates cell user choses to shoot
+ */
+void getShotCell() {
+
+}
+
+/**
+ * @brief Prompts, parses, and validates user ship placement
+ */
+void placeShips() {
+
+}
