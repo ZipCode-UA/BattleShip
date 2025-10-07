7fa1de91775175d81cc24341a1be0c12741ab7a2
Lucas Ergh <lucasergh@gmail.com>
2025-10-06 22:12:30 -0400

    Add Game class with Game.hpp and .cpp files

---
+++ include/Game.hpp
@@ -0,0 +1,20 @@
+/**
+ *  Game.hpp
+ */
+
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "Board.hpp"
+
+class Game {
+public:
+    Game();
+
+    void run();
+private:
+    Board Player1;
+    Board Player2;
+};
+
+#endif
