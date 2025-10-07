4f4d29cb957be1e7bb2fedc22b202ace035fdad7
Lucas Ergh <lucasergh@gmail.com>
2025-10-06 21:51:05 -0400

    Add title message

--- src/BattleShip.cpp
+++ src/BattleShip.cpp
@@ -1,9 +1,16 @@
+/*
+    BattleShip.cpp
+
+    Implimentation of the game BattleShip
+*/
+
 #include <iostream>
 
 int main(int argc, char** argv)
 {
 
-    std::cout << "battle ship you win\n";
+    std::cout << "Welcome to BattleShip!\nPress Enter to Continue ...\n";
+    std::cin.get();
 
     return 0;
 }
