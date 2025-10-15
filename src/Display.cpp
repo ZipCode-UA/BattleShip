d9f6b74bb0dab0bc3f95b8a849018f2348015bc2
Lucas Ergh <LucasErgh@gmail.com>
2025-10-15 16:04:20 -0400

    Add DrawBoard to draw a single board

--- src/Display.cpp
+++ src/Display.cpp
@@ -18,7 +18,72 @@ namespace Display {
      * @brief Draw s cells in gird to standard output
      */
     void DrawBoard(const Board& board, bool showShips) {
-        // Add changes
+        // column labels
+        std::cout << "   ";
+        for (int i = 1; i <= board.boardWidth; ++i) {
+            std::cout << std::to_string(i) << ' ';
+        }
+        std::cout << "\n\n";
+
+        // Top of board's box
+        std::cout << "  ┏";
+        for (int i = 0; i < board.boardWidth - 1; ++i) {
+            std::cout << "━┯";
+        }
+        std::cout << "━┓\n";
+
+        Coords pos;
+        CellTypes curType;
+        for (pos.y = 0; pos.y < board.boardHeight; ++pos.y) {
+
+            // Draw row label
+            const char rowLabel = 'A';
+            std::cout << static_cast<char>(rowLabel + pos.y) << " ┃";
+
+            for (pos.x = 0; pos.x < board.boardWidth; ++pos.x) {
+                // Draw Cell
+                board.getCell(pos, curType);
+                switch (curType)
+                {
+                case CellTypes::Empty:
+                    std::cout << 'E';
+                    break;
+                 case CellTypes::Ship:
+                    if (showShips) std::cout << 'S';
+                    else std::cout << 'E';
+                    break;
+                 case CellTypes::Hit:
+                    std::cout << 'H';
+                    break;
+                 case CellTypes::Miss:
+                    std::cout << 'M';
+                    break;
+                default:
+                    break;
+                }
+
+                if (pos.x == board.boardWidth - 1) std::cout << "┃";
+                else std::cout << "│";
+            }
+
+            std::cout << "\n";
+
+            if (pos.y == board.boardHeight - 1) {
+                // Draw bottom of board's box
+                std::cout << "  ┗";
+                for (int i = 0; i < board.boardWidth - 1; ++i)
+                    std::cout << "━┷";
+                std::cout << "━┛";
+            }
+            else {
+                // Draw inbetween rows
+                std::cout << "  ┠";
+                for (int i = 0; i < board.boardWidth - 1; ++i)
+                    std::cout << "─┼";
+                std::cout << "─┨";
+            }
+            std::cout << "\n";
+        }
     }
 
     /**
